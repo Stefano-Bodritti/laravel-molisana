b6d0a5bb78c732327d4ad46388fe1a51e97a8d8d
Stefano <tepo@hotmail.it>
2021-05-07 16:56:23 +0200

    Header completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,25 @@
     {{-- google fonts --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="{{  asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>La Molisana</title>
   </head>
   <body>
     {{-- intestazione --}}
-    <header></header>
+    <header>
+      <div class="container">
+        <div class="logo_container">
+          <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo">
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">Prodotti</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
     {{-- fine intestazione --}}
     {{-- corpo principale --}}
     <main></main>
