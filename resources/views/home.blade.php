cbe42c595b993f70814987ca64100ec7be78a61f
Stefano <tepo@hotmail.it>
2021-05-07 17:23:54 +0200

    Main completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,52 @@
     </header>
     {{-- fine intestazione --}}
     {{-- corpo principale --}}
-    <main></main>
+    <main>
+      {{-- sezione lunghe --}}
+      <section>
+        <div class="container">
+          <h2>Le lunghe</h2>
+          <div class="boxes">
+            @foreach ($lunghe as $pasta)
+            <div class="box">
+              <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </section>
+      {{-- fine sezione lunghe --}}
+
+      {{-- sezione corte --}}
+      <section>
+        <div class="container">
+          <h2>Le corte</h2>
+          <div class="boxes">
+            @foreach ($corte as $pasta)
+            <div class="box">
+              <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </section>
+      {{-- fine sezione corte --}}
+
+      {{-- sezione cortissime --}}
+      <section>
+        <div class="container">
+          <h2>Le cortissime</h2>
+          <div class="boxes">
+            @foreach ($cortissime as $pasta)
+            <div class="box">
+              <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </section>
+      {{-- fine sezione cortissime --}}
+    </main>
     {{-- fine corpo principale --}}
     {{-- piè di pagina --}}
     <footer></footer>
