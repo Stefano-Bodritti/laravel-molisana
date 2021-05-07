25e84b5929eb9e8e409e6b660cb7c0a2d0143011
Stefano <tepo@hotmail.it>
2021-05-07 17:50:17 +0200

    Suddivisi header e footer in file separati

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,20 +12,7 @@
   </head>
   <body>
     {{-- intestazione --}}
-    <header>
-      <div class="container">
-        <div class="logo_container">
-          <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo">
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#" class="active">Home</a></li>
-            <li><a href="#">Prodotti</a></li>
-            <li><a href="#">News</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    @include('parts.header')
     {{-- fine intestazione --}}
     {{-- corpo principale --}}
     <main>
@@ -76,64 +63,7 @@
     </main>
     {{-- fine corpo principale --}}
     {{-- piè di pagina --}}
-    <footer>
-      <div class="container">
-        <div class="footer_top">
-          <div class="col col-1">
-            <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo">
-            <ul>
-              <li>Ragione sociale: La Molisana S.P.A.</li>
-              <li>Sede legale: Contrada Colle delle Api, 100/A</li>
-              <li>86100 - Campobasso(CB)</li>
-              <li>Pec: lamolisana@pec.it</li>
-              <li>Tel: +39 0874 4981</li>
-              <li>Fax: +39 0874 629584</li>
-              <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
-              <li>commerciale@lamolisana.it</li>
-              <li>export@lamolisana.it</li>
-              <li>numero verde 800818081</li>
-              <li>telefono 3801292455</li>
-            </ul>
-          </div>
-
-          <div class="col col-2">
-            <h4>Pastificio</h4>
-            <ul>
-              <li>Il Pastificio</li>
-              <li>Grano decorticato a pietra</li>
-              <li>Il Molise c'è</li>
-              <li>Filiera Integrata</li>
-              <li>100 anni di pasta</li>
-              <li>Sartoria della pasta</li>
-              <li>Spaghetto Quadrato</li>
-              <li>Le Persone</li>
-            </ul>
-            <h4>Prodotti</h4>
-            <ul>
-              <li>Le Classiche</li>
-              <li>Le Integrali</li>
-              <li>Le Speciali</li>
-              <li>Le Biologiche</li>
-              <li>Le Gluten-Free</li>
-              <li>Le Semole</li>
-              <li>Le Extra di Lusso</li>
-            </ul>
-          </div>
-
-          <div class="col col-3">
-            <h4>Collezione da Chef</h4>
-            <ul>
-              <li>Collezione da Chef</li>
-              <li>Grandi Cucine</li>
-              <li>Biologiche</li>
-              <li>Quadrate</li>
-          </div>
-        </div>
-      </div>
-      <div class="footer_bottom">
-        <img src="{{ asset('images/footer-montagne-bottom.jpg') }}" alt="Montagne">
-      </div>
-    </footer>
+    @include('parts.footer')
     {{-- piè di pagina --}}
   </body>
 </html>
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+  <div class="container">
+    <div class="logo_container">
+      <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#">Prodotti</a></li>
+        <li><a href="#">News</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
