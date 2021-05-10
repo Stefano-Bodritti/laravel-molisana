745ff6dc3dd50ebed01c96e6dbddc3bdb9049888
Stefano <tepo@hotmail.it>
2021-05-10 17:26:10 +0200

    Pagina prodotti completata

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -5,7 +5,14 @@
 @endsection
 
 @section('content')
-  <div class="product_container">
-    {{$pasta['descrizione']}}
+  <div class="product">
+    <div class="chevron"><i class="fas fa-chevron-left"></i></div>
+    <div class="container">
+      <h1>{{ $pasta['titolo'] }}</h1>
+      <img src="{{ $pasta['src-h'] }}" alt="{{ $pasta['titolo'] }} 1">
+      <img src="{{ $pasta['src-p'] }}" alt="{{ $pasta['titolo'] }} 2">
+      <p>{!! $pasta['descrizione'] !!}</p>
+    </div>
+    <div class="chevron"><i class="fas fa-chevron-right"></i></div>
   </div>
 @endsection
