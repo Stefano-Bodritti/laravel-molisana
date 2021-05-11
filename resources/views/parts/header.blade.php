b0c5c13d81d71379f78932579c9fb5705cc1d35e
Stefano <tepo@hotmail.it>
2021-05-11 10:06:12 +0200

    Aggiunto burger menu al header

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -4,6 +4,7 @@
       <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo">
     </div>
     <nav>
+      <div class="burger"><i class="fas fa-bars"></i></div>
       <ul>
         <li><a href="{{ route("homepage") }}" class="{{Route::currentRouteName() == 'homepage' ? 'active' : ''}}">Home</a></li>
         <li><a href="{{ route("prodotti", ['id' => 0]) }}" class="{{Route::currentRouteName() == 'prodotti' ? 'active' : ''}}">Prodotti</a></li>
